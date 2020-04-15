c8dcec0e8bfb17d5337138cb75f330427b5c14ca
ljx <liujiaxin@coding.net>
2020-04-15 18:28:15 +0800

    Initial Commit

---
+++ sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample
+//
+//  Created by jiaxin Liu on 2020/4/15.
+//  Copyright © 2020 jiaxin Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
